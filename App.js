314d0bdc98054d012d5a85e43780bbc4f476cbf9
Amanda <93018313+Ahmandi@users.noreply.github.com>
2022-08-13 10:24:35 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Counter from './components/counter';
+import CounterControls from './components/counterControl';
+
+function App() {
+	return (
+		<div className="App">
+			<Counter />
+			<CounterControls />
+		</div>
+	);
+}
+
+export default App;
